4ea427d032a2c2f3cef90c3590fbfbe8ecd74e43
2kyym <shiaregi279@yahoo.co.jp>
2021-05-18 16:33:05 +0900

    Implement PVC creation in MutatingWebhook

--- api/v1/pod_mutating_webhook.go
+++ api/v1/pod_mutating_webhook.go
@@ -3,9 +3,18 @@ package v1
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"path/filepath"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
@@ -28,7 +37,61 @@ func (a *podAnnotator) Handle(ctx context.Context, req admission.Request) admiss
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	// mutate the fields in pod
+	if pod.Annotations["fluent-pvc.enabled"] != "true" {
+		return admission.Denied(fmt.Sprintf("Pod: %s is not a target for fluent-pvc.", pod.Name))
+	}
+	if pod.Annotations["fluent-pvc.on-create.processed"] == "true" {
+		return admission.Denied(fmt.Sprintf("fluent-pvc is already exist for Pod: %s", pod.Name))
+	}
+
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	pvcClient := clientset.CoreV1().PersistentVolumeClaims(corev1.NamespaceDefault)
+
+	storageClassName := "default-storage-class"
+	pvcName := "fluent-pvc-" + pod.Name + "-" + RandStringRunes(8)
+
+	// _, err = pvcClient.Get(ctx, pvcName, metav1.GetOptions{})
+	// if err == nil {
+	// 	return admission.Denied(fmt.Sprintf("fluent-pvc named %s is already exist", pvcName))
+	// }
+
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: pvcName,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{"ReadWriteOnce"},
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse("8Gi"),
+				},
+			},
+			StorageClassName: &storageClassName,
+		},
+	}
+
+	fmt.Println("Creating PVC...")
+	pvcResult, err := pvcClient.Create(ctx, pvc, metav1.CreateOptions{})
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	fmt.Printf("Created PVC %q.\n", pvcResult.GetObjectMeta().GetName())
 
 	marshaledPod, err := json.Marshal(pod)
 	if err != nil {
@@ -40,4 +103,16 @@ func (a *podAnnotator) Handle(ctx context.Context, req admission.Request) admiss
 func (a *podAnnotator) InjectDecoder(d *admission.Decoder) error {
 	a.decoder = d
 	return nil
+}
+
+func int32Ptr(i int32) *int32 { return &i }
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
