fca45dac72b44b616a0e0cd313f593329c0c6ebb
civitaspo <civitaspo@gmail.com>
2021-05-19 23:23:18 +0900

    kubebuilder create webhook --group core --kind Pod --version v1 --defaulting --programmatic-validation

--- main.go
+++ main.go
@@ -70,6 +70,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FluentPVCOperator")
 		os.Exit(1)
 	}
+	if err = (&corev1.Pod{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Pod")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
