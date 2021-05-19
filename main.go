a7ef85f3605e1543ae972145354bb9dbacef50fd
civitaspo <civitaspo@gmail.com>
2021-05-19 23:21:37 +0900

    Revert "kubebuilder create webhook --group fluent-pvc-operator --kind FluentPVCOperator --version v1alpha1 --defaulting --programmatic-validation"
    This reverts commit e558cd3cd0df84df9296ad85a08734208c70b2cb.

--- main.go
+++ main.go
@@ -70,10 +70,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "FluentPVCOperator")
 		os.Exit(1)
 	}
-	if err = (&fluentpvcoperatorv1alpha1.FluentPVCOperator{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "FluentPVCOperator")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
