11af01b14d14bc6915e862a7d900928fe4438451
2kyym <shiaregi279@yahoo.co.jp>
2021-06-22 16:12:06 +0900

    Move some funcs for e2e test to testutils.go

--- e2e/pod_controller_test.go
+++ e2e/pod_controller_test.go
@@ -13,7 +13,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	fluentpvcv1alpha1 "github.com/st-tech/fluent-pvc-operator/api/v1alpha1"
 	"github.com/st-tech/fluent-pvc-operator/constants"
@@ -123,101 +122,6 @@ func generateTestPodManifest(testPodConfig testPodConfig) *corev1.Pod {
 		pod.Spec.RestartPolicy = testPodConfig.RestartPolicy
 	}
 	return pod
-}
-
-func deleteFluentPVC(ctx context.Context, c client.Client, n string) error {
-	fpvc := &fluentpvcv1alpha1.FluentPVC{}
-	if err := c.Get(ctx, client.ObjectKey{Name: n}, fpvc); err != nil {
-		if apierrors.IsNotFound(err) {
-			return nil
-		}
-		return err
-	}
-	bindings := &fluentpvcv1alpha1.FluentPVCBindingList{}
-	if err := c.List(ctx, bindings); client.IgnoreNotFound(err) != nil {
-		return err
-	}
-	for _, b := range bindings.Items {
-		if !metav1.IsControlledBy(&b, fpvc) {
-			continue
-		}
-		if err := deleteFluentPVCBinding(ctx, c, &b); err != nil {
-			return err
-		}
-	}
-	if controllerutil.ContainsFinalizer(fpvc, constants.FluentPVCFinalizerName) {
-		controllerutil.RemoveFinalizer(fpvc, constants.FluentPVCFinalizerName)
-		if err := c.Update(ctx, fpvc); err != nil {
-			return err
-		}
-	}
-	if err := c.Delete(ctx, fpvc); err != nil {
-		return err
-	}
-	return nil
-}
-
-func deleteFluentPVCBinding(ctx context.Context, c client.Client, b *fluentpvcv1alpha1.FluentPVCBinding) error {
-	if err := deletePVC(ctx, c, b.Spec.PVC.Name, b.Namespace); err != nil {
-		return err
-	}
-	if controllerutil.ContainsFinalizer(b, constants.FluentPVCBindingFinalizerName) {
-		controllerutil.RemoveFinalizer(b, constants.FluentPVCBindingFinalizerName)
-		if err := c.Update(ctx, b); client.IgnoreNotFound(err) != nil {
-			return err
-		}
-	}
-	if err := c.Delete(ctx, b, client.GracePeriodSeconds(0)); client.IgnoreNotFound(err) != nil {
-		return err
-	}
-	return nil
-}
-
-func deletePVC(ctx context.Context, c client.Client, name string, namespace string) error {
-	pvc := &corev1.PersistentVolumeClaim{}
-	if err := c.Get(ctx, client.ObjectKey{Namespace: namespace, Name: name}, pvc); err != nil {
-		if apierrors.IsNotFound(err) {
-			return nil
-		} else {
-			return err
-		}
-	}
-	if controllerutil.ContainsFinalizer(pvc, constants.PVCFinalizerName) {
-		controllerutil.RemoveFinalizer(pvc, constants.PVCFinalizerName)
-		if err := c.Update(ctx, pvc); client.IgnoreNotFound(err) != nil {
-			return err
-		}
-		// if pvc.Status.Phase == corev1.ClaimBound {
-		if err := deletePV(ctx, c, pvc.Spec.VolumeName, pvc.Namespace); err != nil {
-			return err
-		}
-		// }
-	}
-	if err := c.Delete(ctx, pvc, client.GracePeriodSeconds(0)); client.IgnoreNotFound(err) != nil {
-		return err
-	}
-	return nil
-}
-
-func deletePV(ctx context.Context, c client.Client, name string, namespace string) error {
-	pv := &corev1.PersistentVolume{}
-	if err := c.Get(ctx, client.ObjectKey{Namespace: namespace, Name: name}, pv); err != nil {
-		if apierrors.IsNotFound(err) {
-			return nil
-		} else {
-			return err
-		}
-	}
-	if controllerutil.ContainsFinalizer(pv, "kubernetes.io/pv-protection") {
-		controllerutil.RemoveFinalizer(pv, "kubernetes.io/pv-protection")
-		if err := c.Update(ctx, pv); client.IgnoreNotFound(err) != nil {
-			return err
-		}
-	}
-	if err := c.Delete(ctx, pv, client.GracePeriodSeconds(0)); client.IgnoreNotFound(err) != nil {
-		return err
-	}
-	return nil
 }
 
 var _ = Describe("pod_controller", func() {
